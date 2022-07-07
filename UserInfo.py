5c08c265777c2b5c426888ad8e63e49eb453ed73
Eng_A_Cuko <85145285+admac-hub@users.noreply.github.com>
2022-07-06 21:56:23 -0400

    Update UserInfo.py

--- UserInfo.py
+++ UserInfo.py
@@ -3,9 +3,9 @@ from doctest import master
 from tkinter import *
 
 def save_info():
-    Username_info = firstname.get()
+    Username_info = username.get()
     Email_info = Email.get()
-    Password_info = lastname.get()
+    Password_info = password.get()
     GreenScreen_info = GreenScreen.get()
     GsPassword_info = GsPassword.get()
     
@@ -28,9 +28,9 @@ def save_info():
     file.write("\n")
     file.write("The criterias to change your windows  password are:")
     file.write("\n")
-    file.write("1- No Comon names or names related to you")
+    file.write("1- criterias")
     file.write("\n")
-    file.write("2- should be 14 characters long , containing at least one capital letter and numbers")
+    file.write(" write your criteries ")
     file.write("\n")
     file.write("\n")
 
@@ -47,7 +47,7 @@ def save_info():
     file.write("\n")
     file.write("the Criterias are :")
     file.write("\n")
-    file.write("should be 6 characters long , can`t start with number")
+    file.write("criteria")
 
 
 app = Tk()
@@ -64,30 +64,30 @@ heading.pack()
  
 firstname_text = Label(text="Windows Username : ")
 Email_text = Label(text="Email : ")
-lastname_text = Label(text="Password : ")
+password_text = Label(text="Password : ")
 GreenScreen_text = Label(text="Green Screen Username : ")
 GsPassword_text = Label(text="Green Screen Password : ")
  
 firstname_text.place(x=15,y=80)
 Email_text.place(x=15,y=140)
-lastname_text.place(x=15,y=200)
+password_text.place(x=15,y=200)
 GreenScreen_text.place(x=15,y=260)
 GsPassword_text.place(x=15,y=320)
 
 firstname = StringVar()
-lastname = StringVar()
+password = StringVar()
 Email = StringVar()
 GreenScreen = StringVar()
 GsPassword = StringVar()
 
-first_name_entry = Entry(textvariable=firstname,width="30")
-last_name_entry = Entry(textvariable=lastname,width="30")
+username_entry = Entry(textvariable=firstname,width="30")
+password_name_entry = Entry(textvariable=lastname,width="30")
 Email_entry = Entry(textvariable=Email,width="30")
 GreenScreen_entry = Entry(textvariable=GreenScreen,width="30")
 GsPassword_entry = Entry(textvariable=GsPassword,width="30")
  
-first_name_entry.place(x=15,y=100)
-last_name_entry.place(x=15,y=160)
+username_entry.place(x=15,y=100)
+Password_entry.place(x=15,y=160)
 Email_entry.place(x=15,y=220)
 GreenScreen_entry.place(x=15,y=280)
 GsPassword_entry.place(x=15,y=340)
